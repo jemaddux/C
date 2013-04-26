f89e5be02ad661dda21ea18e36ab6b34d510dabb
John Maddux <jemaddux@gmail.com>
2013-04-26 16:00:02 -0600

    First bit done

--- AGoodStart/main.c
+++ AGoodStart/main.c
@@ -1,15 +1,14 @@
-//
-//  main.c
-//  AGoodStart
-//
-//  Created by John Maddux on 4/26/13.
-//  Copyright (c) 2013 John Maddux. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-
+int main(int argc, const char * argv[])
+{
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+       Maybe it needs a rewrite. */
+    
+    return 0;
    
 }
 
