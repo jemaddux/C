a209fbea3cbe9569b70d891cd4541b3f3bef5e68
John Maddux <jemaddux@gmail.com>
2013-04-26 15:37:10 -0600

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by John Maddux on 4/26/13.
+//  Copyright (c) 2013 John Maddux. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
