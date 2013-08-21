860fb879977a30fbdb65e03e7a8ddb6fb1ef70d7
John Maddux <jemaddux@gmail.com>
2013-08-20 20:10:48 -0400

    Done with challenge

--- TwoFloats/main.c
+++ TwoFloats/main.c
@@ -10,9 +10,13 @@
 
 int main(int argc, const char * argv[])
 {
-
+    float one = 2.34;
+    float two = 3.45;
+    
+    double dubble = one + two;
+    
     // insert code here...
-    printf("Hello, World!\n");
+    printf("This is the result: %f", dubble);
     return 0;
 }
 
