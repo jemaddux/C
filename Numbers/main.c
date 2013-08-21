89c0899e5f67679d9d4fdd70fc9ec590f3fc990a
John Maddux <jemaddux@gmail.com>
2013-08-21 19:10:54 -0400

    DONE

--- Numbers/main.c
+++ Numbers/main.c
@@ -11,11 +11,28 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    long derp = 88123123;
-    printf("derp the integer is %ld.\n", derp);
-    printf("In octal, derp is %lo.\n", derp);
-    printf("In hexadecimal, derp is %lx.\n", derp);
+    int derp = 88;
+    printf("derp the integer is %d.\n", derp);
+    printf("In octal, derp is %o.\n", derp);
+    printf("In hexadecimal, derp is %x.\n", derp);
+    
+    long derp2 = 88123123;
+    printf("derp the integer is %ld.\n", derp2);
+    printf("In octal, derp is %lo.\n", derp2);
+    printf("In hexadecimal, derp is %lx.\n", derp2);
+
+    unsigned long x = 255;
+    printf("x is %lu.\n", x);
+    
+    //Ocatl and hex already assumed the number was unsigned
+    printf("In octal, x is %lo.\n", x);
+    printf("In hexadecimal, x is %lx.\n", x);
+    
+    printf("# * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+    
+    int r = 4;
+    r++;
+    
     return 0;
 }
 
