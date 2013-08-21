80ad84c33a5790db14ddb6e0524ae660e028f522
John Maddux <jemaddux@gmail.com>
2013-08-20 20:04:01 -0400

    Cooking a turkey, turkey

--- Turkey/main.c
+++ Turkey/main.c
@@ -10,9 +10,27 @@
 
 int main(int argc, const char * argv[])
 {
+    // Delcare the variable called 'weight' of type float
+    float weight;
+    
+    // Put a number in that variable
+    weight = 14.2;
+    
+    //Log it to the user
+    printf("The turkey weighs %f./n", weight);
+    
+    //Decalre another variable of type float
+    float cookingTime;
+    
+    // Calculate the cooking time and store it in the variable
+    // In this case, '*' mean 'multiplied by'
+    cookingTime = 15.0 + 15.0 * weight;
+    
+    // Log that to the user
+    printf("Cook it for %f minutes.\n", cookingTime);
+    
+    //End this function and indicate success
 
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
 
