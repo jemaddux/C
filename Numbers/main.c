7d11fe71478819fe255386d88d0a56621c53f5b2
John Maddux <jemaddux@gmail.com>
2013-08-21 18:53:30 -0400

    Derp the number is now many things

--- Numbers/main.c
+++ Numbers/main.c
@@ -13,7 +13,9 @@ int main(int argc, const char * argv[])
 
     // insert code here...
     int derp = 88;
-    printf("derp the integer is %d.",derp);
+    printf("derp the integer is %d.\n", derp);
+    printf("In octal, derp is %o.\n", derp);
+    printf("In hexadecimal, derp is %x.\n", derp);
     return 0;
 }
 
