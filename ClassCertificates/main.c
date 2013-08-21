76d823a511aaaffe79cc1b20ed330f7d513df614
John Maddux <jemaddux@gmail.com>
2013-08-20 20:42:50 -0400

    Done

--- ClassCertificates/main.c
+++ ClassCertificates/main.c
@@ -8,11 +8,17 @@
 
 #include <stdio.h>
 
+void congratulateStudent(char *student, char *course, int numDays)
+{
+    printf("%s has done as much %s Programming as I could fit into %d days.\n", student, course, numDays);
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    congratulateStudent("Mark", "Cocoa", 5);
+    congratulateStudent("Bo", "Objective-C", 2);
+    congratulateStudent("Mike", "Python", 5);
+    congratulateStudent("Ted", "iOS", 500);
     return 0;
 }
 
