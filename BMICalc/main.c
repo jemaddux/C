2e8547f98217b5f70b98ae869ebf342c04358bac
John Maddux <jemaddux@gmail.com>
2013-08-24 23:20:25 -0400

    Initial Commit

---
+++ BMICalc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by John Maddux on 8/24/13.
+//  Copyright (c) 2013 John Maddux. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
