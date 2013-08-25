54a2b7524a89ecc0242cadd23a60a730059da5cb
John Maddux <jemaddux@gmail.com>
2013-08-25 11:04:51 -0400

    Added a BMI function

--- BMICalc/main.c
+++ BMICalc/main.c
@@ -9,19 +9,24 @@
 #include <stdio.h>
 
 // Here is the declaration of the struct Person
-struct Person {
+typedef struct {
     float heightInMeters;
     int weightInKilos;
-};
+} Person;
+
+float bodyMassIndex(Person p)
+{
+    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+}
 
 int main(int argc, const char * argv[])
 {
     
-    struct Person person;
+    Person person;
     person. weightInKilos = 96;
     person.heightInMeters = 1.8;
-    printf("person weighs %i kilograms\n", person.weightInKilos);
-    printf("person is %.2f meters tall\n", person.heightInMeters);
+    float bmi = bodyMassIndex(person);
+    printf("person has a BMI of %.2f\n", bmi);
     return 0;
 }
 
